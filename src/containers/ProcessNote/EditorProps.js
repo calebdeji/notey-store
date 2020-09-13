94e73b9dbf5541e502817da059b7f06211f386bb
calebdeji <calebdeji06@gmail.com>
2020-09-13 10:19:03 +0100

    add more text formatting options

--- src/containers/ProcessNote/EditorProps.js
+++ src/containers/ProcessNote/EditorProps.js
@@ -1,25 +1,28 @@
 export const editorFormats = [
     'header',
-    'font',
-    'size',
     'bold',
     'italic',
     'underline',
     'strike',
-    'blockquote',
     'list',
     'bullet',
     'indent',
-    'link',
     'image',
     'color',
+    'font',
 ];
 export const editorModules = {
     toolbar: [
-        [{ header: [1, 2, false] }],
-        ['bold', 'italic', 'underline', 'strike', 'blockquote', 'color'],
-        [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
-        ['link', 'image'],
-        ['clean'],
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        ['bold', 'italic', 'underline', 'strike'],
+
+        [{ list: 'ordered' }, { list: 'bullet' }],
+        [{ indent: '-1' }, { indent: '+1' }],
+
+        [{ color: [] }],
+        [{ font: [] }],
     ],
+    clipboard: {
+        matchVisual: false,
+    },
 };
